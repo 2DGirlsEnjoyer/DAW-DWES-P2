b019caa17ea64eff469242323195245246ec8996
sergio <smasot@campus.eug.es>
2022-09-22 10:28:45 +0200

    he actualizado el ejercicio 12, la segunda parte, porque tenia las condicones dentro del for y no me habia dado cuenta, uwu <3

--- P2 PHP Básico/EX_12/index.php
+++ P2 PHP Básico/EX_12/index.php
@@ -15,20 +15,19 @@ function numero_primo_wilson($num1){
 }
 
 function numero_primo($num1){
-    $contador = 1;
+    $contador = 0;
     for ($i=1; $i<=$num1; $i++){
         if ($num1%$i==0){
             $contador++;
-            if($contador==2){
-                return 'true';
-            }
-            else {
-                return 'false';
-            }
         }
+    }
+    if ($contador==2){
+        return "true";
+    } else {
+        return "false";
     }
 }
 
 echo numero_primo_wilson(21.0).'</br>';
-echo numero_primo(83.0);
+echo numero_primo(11.0);
 
