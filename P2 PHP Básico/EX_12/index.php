27c21f005fe5521465ba51825d76973e80ce349b
sergio <smasot@campus.eug.es>
2022-09-22 10:13:43 +0200

    marikaaaaaa que aksco

--- P2 PHP Básico/EX_12/index.php
+++ P2 PHP Básico/EX_12/index.php
@@ -1,15 +1,34 @@
 <?php
-function numero_primo($num1){
+function numero_primo_wilson($num1){
     $result =1;
-    for ($i=$num1-1; $i==1; $i--){
+    for ($i=$num1-1; $i>1; $i--){
         $result *= $i;
     }
-    $result2 = $result+1;
-    $result3 = $result2/$num1;
-    if ($result3 % $num1 == 0){
-        return true;
+    $result++;
+
+    if (($result % $num1) == 0.0){
+        return "true";
     } else {
-        return false;
+        return "false";
+    }
+
+}
+
+function numero_primo($num1){
+    $contador = 1;
+    for ($i=1; $i<=$num1; $i++){
+        if ($num1%$i==0){
+            $contador++;
+            if($contador==2){
+                return 'true';
+            }
+            else {
+                return 'false';
+            }
+        }
     }
 }
-echo numero_primo(3);
+
+echo numero_primo_wilson(21.0).'</br>';
+echo numero_primo(83.0);
+
