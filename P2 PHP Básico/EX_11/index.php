0503cd4b83b0a3c9e6825ddae3a6ba5847599a20
sergio <smasot@campus.eug.es>
2022-09-22 10:32:37 +0200

    he cambiado un if, para que salga en caso de que todas sean minus, tambien lo notifique, y no unicamente en caso de que sean todas mayus

--- P2 PHP Básico/EX_11/index.php
+++ P2 PHP Básico/EX_11/index.php
@@ -3,7 +3,10 @@
 function es_mayus($palabra){
     if ($palabra == $str = strtoupper($palabra)){
         $area = 'todos son mayus';
-    } else {
+    } elseif ($palabra == $str = strtolower($palabra)){
+        $area = 'todos son minus';
+    }
+    else {
         $area = 'no todos son mayus';
     }
     return $area;
